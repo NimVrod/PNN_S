8685bb1dbf8d8e60c859158fbd1b337d882d670a
NimVrod <hdec0759@gmail.com>
2026-03-24 18:59:33 +0100

    Initial python commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pnn
+
+def main():
+    print(np.abs(-1))
+    print("Hello from pnn-s!")
+    pn = pnn.PNN()
+
+if __name__ == "__main__":
+    main()
---
+++ pnn.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+class PNN:
+
+    def __init__(self) -> None:
+        print("PNN")
+        pass
